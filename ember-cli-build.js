d89597dce63ef9d2eb98d7272785433f1000fcdb
Julian Tescher <jatescher@gmail.com>
2020-05-05 11:41:09 -0700

    Add purgecss to trim build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,16 +1,41 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      './app/index.html',
+      './app/components/**/*.hbs',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+};
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
       exclude: ['tokenAsset-1.png', 'tokenAsset-2.png', 'tokenAsset-3.png']
     },
-    sassOptions: {
-      includePaths: [
-        'node_modules/bootstrap/scss'
-      ]
+    postcssOptions: {
+      compile: {
+        extension: 'scss',
+        enabled: true,
+        parser: require('postcss-scss'),
+        plugins: [
+          {
+            module: require('@csstools/postcss-sass'),
+            options: {
+              includePaths: [
+                'node_modules/bootstrap/scss',
+              ],
+            },
+          },
+          ...(isProduction ? [purgeCSS] : []),
+        ]
+      },
     },
   });
 
