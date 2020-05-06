9dc208b06f2f11935f285b803efab9459459ef92
Julian Tescher <jatescher@gmail.com>
2020-05-06 15:07:20 -0700

    Add mobile nav collapse

--- app/components/main-navbar.js
+++ app/components/main-navbar.js
@@ -1,12 +1,19 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class MainNavbarComponent extends Component {
   @service bitski;
+  @tracked expanded = false;
 
   get isLoggedIn() {
     return this.bitski.isLoggedIn;
+  }
+
+  @action
+  toggleExpanded() {
+    this.expanded = !this.expanded;
   }
 
   @action
