b35ecae3307d6fe42d02fa32129ce5803a12c2e6
Julian Tescher <jatescher@gmail.com>
2020-05-11 15:55:12 -0700

    Switch to simpler account check

--- app/services/bitski.js
+++ app/services/bitski.js
@@ -20,26 +20,13 @@ export default class BitskiService extends Service {
       return user;
     } else {
       // Sessions do not always include accounts, e.g. newly created user accounts.
-      let accessToken = await this._bitski.getCurrentAccessToken();
-      let accounts = await this.fetchAccounts(accessToken);
+      let accounts = await this._bitski
+        .getProvider({ disableBlockTracking: true })
+        .send('eth_accounts', []);
       user.accounts = accounts;
       this._bitski.authProvider.userStore.set(user);
       return user;
     }
-  }
-
-  async fetchAccounts(accessToken) {
-    let response = await fetch('https://api.bitski.com/v1/web3/mainnet', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`
-      },
-      body: '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}'
-    });
-    let json = await response.json();
-
-    return json.result;
   }
 
   logOut() {
